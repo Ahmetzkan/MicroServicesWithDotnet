f00ed1ad0161dc0c7ca4b636c5b4d1751bcf19a0
Ahmet Özkan <ahmet.ozkan98@hotmail.com>
2025-03-24 00:17:14 +0300

    Added Dockerfile and docker-compose configuration for container setup and volume management

---
+++ WebApplicationForDocker/Controllers/FilesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationForDocker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> UploadFile(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+            {
+                return BadRequest("File is empty");
+            }
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images", file.FileName);
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+            return Ok();
+        }
+    }
+}
